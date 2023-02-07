3d755377768b7672e201a10318c62b6ae1829dc9
Oshan Hamal <oshanhamal3@gmail.com>
2023-02-07 11:00:49 +0545

    generic-relationship

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItems(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    """ 
+    Generic Relationship
+    1. type(video, article, ...) 
+    2. ID   
+    """
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_id = models.PositiveIntegerField()
+    # object
+    content_object = GenericForeignKey()
